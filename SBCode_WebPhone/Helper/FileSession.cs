f928ca0ddbaf18121ed3062ce726e902ae676b93
chanvinh41@gmail.com <chanvinh41@gmail.com>
2021-07-31 10:53:50 +0700

    all

--- SBCode_WebPhone/Helper/FileSession.cs
+++ SBCode_WebPhone/Helper/FileSession.cs
@@ -6,7 +6,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SBCode_WebPhone.Helper
-{
+{//
     public static class FileSession
     {
         public static string ToVnd(this double giaTri)
